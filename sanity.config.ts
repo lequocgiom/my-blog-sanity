3df95f72f94243124fd8eafc7444f7c77f7c038e
Trung.l <trung.l@binance.com>
2022-12-21 13:56:18 +0700

    first commit

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+import {codeInput} from '@sanity/code-input'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +11,7 @@ export default defineConfig({
   projectId: 't1cl28s8',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool(), visionTool(), codeInput()],
 
   schema: {
     types: schemaTypes,
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,100 @@
-export const schemaTypes = []
+export const schemaTypes = [
+  {
+    name: 'author',
+    type: 'document',
+    title: 'Author',
+    fields: [
+      {
+        name: 'name',
+        title: 'Name',
+        type: 'string',
+      },
+      {
+        name: 'avatar',
+        title: 'Avatar',
+        type: 'image',
+      },
+    ],
+  },
+  {
+    name: 'blog',
+    type: 'document',
+    title: 'Blog',
+    fields: [
+      {
+        name: 'title',
+        type: 'string',
+        title: 'Title',
+      },
+      {
+        name: 'subtitle',
+        type: 'string',
+        title: 'Subtitle',
+      },
+      {
+        name: 'coverImage',
+        title: 'Cover Image',
+        type: 'image',
+        fields: [
+          {
+            type: 'text',
+            name: 'alt',
+            title: 'Description',
+          },
+        ],
+      },
+      {
+        name: 'content',
+        title: 'Content',
+        type: 'array',
+        of: [
+          {
+            type: 'block',
+          },
+          {
+            type: 'image',
+            fields: [
+              {
+                type: 'text',
+                name: 'alt',
+                title: 'Description',
+                options: {
+                  isHighlighted: true,
+                },
+              },
+            ],
+            options: {
+              hotspot: true,
+            },
+          },
+
+          {
+            type: 'code',
+            options: {
+              withFilename: true,
+            },
+          },
+        ],
+      },
+      {
+        name: 'date',
+        title: 'Date',
+        type: 'datetime',
+        validation: (Rule: {required: () => any}) => Rule.required(),
+      },
+      {
+        name: 'author',
+        title: 'Author',
+        type: 'reference',
+        to: [{type: 'author'}],
+        validation: (Rule: {required: () => any}) => Rule.required(),
+      },
+      {
+        name: 'slug',
+        type: 'slug',
+        title: 'Slug',
+        validation: (Rule: {required: () => any}) => Rule.required(),
+      },
+    ],
+  },
+]
